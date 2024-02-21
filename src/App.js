d3239ba33f3b5105552203bb517cce4c0be45373
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2024-02-20 21:28:05 -0500

    react and front end libraries update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  let [counter, changeCounter] = useState(1);
+
+  useEffect(() => {
+    window.changeCounter = changeCounter;
+
+    return () => {
+      window.changeCounter = undefined;
+    };
+  }, [changeCounter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
     </div>
   );
 }
